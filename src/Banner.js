d6ea192ba928312ae57a27aa94d4d702765be3cf
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-03-21 14:31:20 +0500

    pushing initially completed banner

---
+++ src/Banner.js
@@ -0,0 +1,44 @@
+import { Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import './Banner.css';
+import Search from './Search';
+
+function Banner() {
+    const [showSearch, setShowSearch] = useState(false)
+
+
+    return (
+        <div className='banner'>
+            
+            <div className='banner__search'>
+                
+                {showSearch && <Search/>}
+                {/*if show picker state becomes true, then search part will run*/}
+                {/*we do this by making setShowSearch((!showSearch) on clicking this button */}
+                
+                <Button 
+                 variant="outlined"
+                 className='banner__searchButton'
+                 onClick={()=>setShowSearch(!showSearch)}>
+                     {showSearch ? "Hide" : "Search Dates"}
+                </Button>
+            </div>
+
+
+            <div className='banner__info'>
+            
+                <h1>Get out and stretch your imagination</h1>
+                <h5>
+                    Plan a different kind of getaway to uncover the hidden gems near you.
+                </h5>
+                <Button
+                 variant="outlined" >
+                     Explore Nearby
+                </Button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Banner
---
+++ src/Search.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import './Search.css';
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { Button } from '@material-ui/core';
+import PeopleIcon from '@material-ui/icons/People';
+
+//DATE IMPORT
+import { DateRangePicker } from 'react-date-range';
+
+
+
+//DATE PICKER
+// npm i react-date-range
+// npm i date-fns 
+function Search() {
+    
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    const selectionRange = {
+        startDate:startDate,
+        endDate:endDate,
+        key:"selection",
+    };
+
+    //Function for date Selection
+    function handleSelect(ranges) {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+
+
+    return (
+        <div className='search'>
+            
+            <DateRangePicker 
+            ranges={[selectionRange]}
+            onChange={handleSelect}
+             />
+        
+            <h2>
+                Number of guests
+                <PeopleIcon/>
+            </h2>
+
+            <input 
+             min={0}
+             defaultValue={2}
+             type="number"
+             />
+
+             <Button>
+                 Airbnb
+             </Button>
+        
+        </div>
+    )
+}
+
+export default Search
