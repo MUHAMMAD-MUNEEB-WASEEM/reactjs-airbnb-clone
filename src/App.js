11568cb524b62c5e754c8617e2674c112a813b9e
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-03-22 18:35:01 +0500

    adding search page and search result component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
+
 import Header from './Header';
 import './App.css';
 import Home from './Home';
+import Footer from './Footer';
+
+//FOR ROUTING
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import SearchPage from './SearchPage';
 
 function App() {
   return (
 
     //BEM
     <div className="app">
-      <Header/>
-      <Home/>
+      <Router>
+        {/*Footer and Header always their so thats why out of switchs*/}
+        <Header/>
+        
+        {/*for routing, we use switch and provide different routes in it*/}
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
 
-  {/*Home*/}
-    {/*Header*/}
-
-    {/*Banner*/}
-      {/*Search*/}
-
-    {/*cards*/}
-
-    {/*Footer*/}
-
-
-
-
+        {/*always place default page(home) at bottom*/}
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        
+        <Footer/>
+      </Router>
 
     </div>
   );
